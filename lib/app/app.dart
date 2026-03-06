98cc12c0b02a61b88ed9a3b722298bde302f3978
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-06 18:14:21 +0200

    Inject the auth service into the application shell

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:toastification/toastification.dart';
 
 import '../core/theme/app_theme.dart';
+import '../features/auth/application/auth_service.dart';
+import '../features/auth/application/auth_service_contract.dart';
 import '../features/auth/presentation/pages/login_page.dart';
 
 class BudgetifyApp extends StatelessWidget {
-  const BudgetifyApp({super.key});
+  const BudgetifyApp({super.key, this.authService});
+
+  static final AuthServiceContract _defaultAuthService =
+      AuthService.createDefault();
+
+  final AuthServiceContract? authService;
 
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.dark();
+    final resolvedAuthService = authService ?? _defaultAuthService;
 
     return ToastificationWrapper(
       child: MaterialApp(
@@ -18,7 +26,7 @@ class BudgetifyApp extends StatelessWidget {
         theme: theme,
         darkTheme: theme,
         themeMode: ThemeMode.dark,
-        home: const LoginPage(),
+        home: LoginPage(authService: resolvedAuthService),
       ),
     );
   }
