a880a4a49ae3366aa17ceec2f18de122d66a86c3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-29 20:02:36 +0200

    Add client for PATCH /users/me profile updates

---
+++ lib/features/users/data/services/users_api_service.dart
@@ -0,0 +1,28 @@
+import '../../../../core/network/api_client.dart';
+import '../../../auth/data/models/auth_user.dart';
+import '../routes/users_api_routes.dart';
+
+class UsersApiService {
+  UsersApiService({
+    required ApiClient apiClient,
+    required UsersApiRoutes routes,
+  }) : _apiClient = apiClient,
+       _routes = routes;
+
+  final ApiClient _apiClient;
+  final UsersApiRoutes _routes;
+
+  Future<AuthUser> updateCurrentUserNames({
+    required String accessToken,
+    required String firstName,
+    required String lastName,
+  }) async {
+    final json = await _apiClient.patchJson(
+      _routes.me,
+      headers: <String, String>{'Authorization': 'Bearer $accessToken'},
+      body: <String, dynamic>{'firstName': firstName, 'lastName': lastName},
+    );
+
+    return AuthUser.fromJson(json);
+  }
+}
