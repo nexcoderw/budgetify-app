75c88c8111b02eb5e7763ff67da0c8d1f43746af
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-07 20:17:59 +0200

    Update app/lib/app/app.dart listen for partnership invite deep links

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:toastification/toastification.dart';
 
@@ -5,8 +9,10 @@ import '../core/theme/app_theme.dart';
 import '../features/auth/application/auth_service.dart';
 import '../features/auth/application/auth_service_contract.dart';
 import '../features/auth/presentation/pages/login_page.dart';
+import '../features/partnerships/application/partnership_invite_link_store.dart';
+import '../features/partnerships/presentation/partnership_view_utils.dart';
 
-class BudgetifyApp extends StatelessWidget {
+class BudgetifyApp extends StatefulWidget {
   const BudgetifyApp({super.key, this.authService});
 
   static final AuthServiceContract _defaultAuthService =
@@ -15,9 +21,58 @@ class BudgetifyApp extends StatelessWidget {
   final AuthServiceContract? authService;
 
   @override
+  State<BudgetifyApp> createState() => _BudgetifyAppState();
+}
+
+class _BudgetifyAppState extends State<BudgetifyApp> {
+  StreamSubscription<Uri>? _appLinkSubscription;
+
+  AuthServiceContract get _resolvedAuthService =>
+      widget.authService ?? BudgetifyApp._defaultAuthService;
+
+  @override
+  void initState() {
+    super.initState();
+    _initInviteLinks();
+  }
+
+  @override
+  void dispose() {
+    _appLinkSubscription?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _initInviteLinks() async {
+    if (kIsWeb) {
+      return;
+    }
+
+    final appLinks = AppLinks();
+    final initialUri = await appLinks.getInitialLink();
+    _handleIncomingInviteUri(initialUri);
+
+    _appLinkSubscription = appLinks.uriLinkStream.listen(
+      _handleIncomingInviteUri,
+      onError: (_) {},
+    );
+  }
+
+  void _handleIncomingInviteUri(Uri? uri) {
+    if (uri == null) {
+      return;
+    }
+
+    final token = extractInviteTokenFromUri(uri);
+    if (token == null) {
+      return;
+    }
+
+    PartnershipInviteLinkStore.instance.stageInviteToken(token);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final theme = AppTheme.dark();
-    final resolvedAuthService = authService ?? _defaultAuthService;
 
     return ToastificationWrapper(
       child: MaterialApp(
@@ -26,7 +81,7 @@ class BudgetifyApp extends StatelessWidget {
         theme: theme,
         darkTheme: theme,
         themeMode: ThemeMode.dark,
-        home: LoginPage(authService: resolvedAuthService),
+        home: LoginPage(authService: _resolvedAuthService),
       ),
     );
   }
