cdfff419fc2f01d53d056c84d518622a3d4abdd7
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-07 18:24:22 +0200

    Add lib/features/savings/data/routes/savings_api_routes.dart savings route map

---
+++ lib/features/savings/data/routes/savings_api_routes.dart
@@ -0,0 +1,11 @@
+class SavingsApiRoutes {
+  const SavingsApiRoutes._();
+
+  static const instance = SavingsApiRoutes._();
+
+  static const String _base = '/api/v1/savings';
+
+  String get list => _base;
+
+  String byId(String savingId) => '$_base/$savingId';
+}
