a5bdb03d27df0b55abc254056bba7ac857ba5329
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-20 22:11:30 +0200

    Add saving currency fields to Flutter model

--- lib/features/savings/data/models/saving_entry.dart
+++ lib/features/savings/data/models/saving_entry.dart
@@ -5,6 +5,8 @@ class SavingEntry {
     required this.id,
     required this.label,
     required this.amount,
+    required this.currency,
+    required this.amountRwf,
     required this.date,
     required this.note,
     required this.stillHave,
@@ -18,6 +20,11 @@ class SavingEntry {
       id: json['id'] as String,
       label: json['label'] as String,
       amount: (json['amount'] as num).toDouble(),
+      currency: SavingCurrencyCode.fromApi(
+        json['currency'] as String? ?? 'RWF',
+      ),
+      amountRwf: ((json['amountRwf'] as num?) ?? (json['amount'] as num))
+          .toDouble(),
       date: DateTime.parse(json['date'] as String).toLocal(),
       note: json['note'] as String?,
       stillHave: json['stillHave'] as bool? ?? true,
@@ -32,10 +39,35 @@ class SavingEntry {
   final String id;
   final String label;
   final double amount;
+  final SavingCurrencyCode currency;
+  final double amountRwf;
   final DateTime date;
   final String? note;
   final bool stillHave;
   final CreatedBySummary? createdBy;
   final DateTime createdAt;
   final DateTime updatedAt;
+}
+
+enum SavingCurrencyCode {
+  rwf,
+  usd;
+
+  String get apiValue => switch (this) {
+    rwf => 'RWF',
+    usd => 'USD',
+  };
+
+  String get displayName => switch (this) {
+    rwf => 'RWF',
+    usd => 'USD',
+  };
+
+  static SavingCurrencyCode fromApi(String value) {
+    return switch (value) {
+      'RWF' => SavingCurrencyCode.rwf,
+      'USD' => SavingCurrencyCode.usd,
+      _ => throw StateError('Unsupported saving currency: $value'),
+    };
+  }
 }
