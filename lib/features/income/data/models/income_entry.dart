259d006330d42a666dcd3c56e48edec321e465aa
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-21 18:05:21 +0200

    Add income allocation summary and detail models

--- lib/features/income/data/models/income_entry.dart
+++ lib/features/income/data/models/income_entry.dart
@@ -10,6 +10,9 @@ class IncomeEntry {
     required this.category,
     required this.date,
     required this.received,
+    required this.allocatedToSavingsRwf,
+    required this.remainingAvailableRwf,
+    required this.allocationStatus,
     required this.createdBy,
     required this.createdAt,
     required this.updatedAt,
@@ -26,6 +29,16 @@ class IncomeEntry {
       category: IncomeCategory.fromApi(json['category'] as String),
       date: DateTime.parse(json['date'] as String).toLocal(),
       received: json['received'] as bool? ?? false,
+      allocatedToSavingsRwf:
+          (json['allocatedToSavingsRwf'] as num? ?? 0).toDouble(),
+      remainingAvailableRwf:
+          ((json['remainingAvailableRwf'] as num?) ??
+                  (json['amountRwf'] as num?) ??
+                  (json['amount'] as num))
+              .toDouble(),
+      allocationStatus: IncomeAllocationStatus.fromApi(
+        json['allocationStatus'] as String? ?? 'UNALLOCATED',
+      ),
       createdBy: (json['createdBy'] as Map<String, dynamic>?) != null
           ? CreatedBySummary.fromJson(json['createdBy'] as Map<String, dynamic>)
           : null,
@@ -42,9 +55,155 @@ class IncomeEntry {
   final IncomeCategory category;
   final DateTime date;
   final bool received;
+  final double allocatedToSavingsRwf;
+  final double remainingAvailableRwf;
+  final IncomeAllocationStatus allocationStatus;
   final CreatedBySummary? createdBy;
   final DateTime createdAt;
   final DateTime updatedAt;
+}
+
+class IncomeSummary {
+  const IncomeSummary({
+    required this.totalIncomeRwf,
+    required this.receivedIncomeRwf,
+    required this.pendingIncomeRwf,
+    required this.totalExpensesRwf,
+    required this.totalSavingsBalanceRwf,
+    required this.availableMoneyNowRwf,
+    required this.totalIncomeCount,
+    required this.receivedIncomeCount,
+    required this.pendingIncomeCount,
+  });
+
+  factory IncomeSummary.fromJson(Map<String, dynamic> json) {
+    return IncomeSummary(
+      totalIncomeRwf: (json['totalIncomeRwf'] as num? ?? 0).toDouble(),
+      receivedIncomeRwf: (json['receivedIncomeRwf'] as num? ?? 0).toDouble(),
+      pendingIncomeRwf: (json['pendingIncomeRwf'] as num? ?? 0).toDouble(),
+      totalExpensesRwf: (json['totalExpensesRwf'] as num? ?? 0).toDouble(),
+      totalSavingsBalanceRwf:
+          (json['totalSavingsBalanceRwf'] as num? ?? 0).toDouble(),
+      availableMoneyNowRwf:
+          (json['availableMoneyNowRwf'] as num? ?? 0).toDouble(),
+      totalIncomeCount: json['totalIncomeCount'] as int? ?? 0,
+      receivedIncomeCount: json['receivedIncomeCount'] as int? ?? 0,
+      pendingIncomeCount: json['pendingIncomeCount'] as int? ?? 0,
+    );
+  }
+
+  final double totalIncomeRwf;
+  final double receivedIncomeRwf;
+  final double pendingIncomeRwf;
+  final double totalExpensesRwf;
+  final double totalSavingsBalanceRwf;
+  final double availableMoneyNowRwf;
+  final int totalIncomeCount;
+  final int receivedIncomeCount;
+  final int pendingIncomeCount;
+}
+
+class IncomeSavingAllocation {
+  const IncomeSavingAllocation({
+    required this.id,
+    required this.savingId,
+    required this.savingLabel,
+    required this.transactionId,
+    required this.transactionDate,
+    required this.amount,
+    required this.currency,
+    required this.amountRwf,
+    required this.note,
+    required this.isReversed,
+    required this.isReversal,
+    required this.reversedByTransactionId,
+  });
+
+  factory IncomeSavingAllocation.fromJson(Map<String, dynamic> json) {
+    return IncomeSavingAllocation(
+      id: json['id'] as String,
+      savingId: json['savingId'] as String,
+      savingLabel: json['savingLabel'] as String,
+      transactionId: json['transactionId'] as String,
+      transactionDate: DateTime.parse(
+        json['transactionDate'] as String,
+      ).toLocal(),
+      amount: (json['amount'] as num).toDouble(),
+      currency: CurrencyCode.fromApi(json['currency'] as String? ?? 'RWF'),
+      amountRwf: (json['amountRwf'] as num? ?? 0).toDouble(),
+      note: json['note'] as String?,
+      isReversed: json['isReversed'] as bool? ?? false,
+      isReversal: json['isReversal'] as bool? ?? false,
+      reversedByTransactionId: json['reversedByTransactionId'] as String?,
+    );
+  }
+
+  final String id;
+  final String savingId;
+  final String savingLabel;
+  final String transactionId;
+  final DateTime transactionDate;
+  final double amount;
+  final CurrencyCode currency;
+  final double amountRwf;
+  final String? note;
+  final bool isReversed;
+  final bool isReversal;
+  final String? reversedByTransactionId;
+}
+
+class IncomeDetail extends IncomeEntry {
+  const IncomeDetail({
+    required super.id,
+    required super.label,
+    required super.amount,
+    required super.currency,
+    required super.amountRwf,
+    required super.category,
+    required super.date,
+    required super.received,
+    required super.allocatedToSavingsRwf,
+    required super.remainingAvailableRwf,
+    required super.allocationStatus,
+    required super.createdBy,
+    required super.createdAt,
+    required super.updatedAt,
+    required this.allocationCount,
+    required this.savingAllocations,
+  });
+
+  factory IncomeDetail.fromJson(Map<String, dynamic> json) {
+    final base = IncomeEntry.fromJson(json);
+
+    return IncomeDetail(
+      id: base.id,
+      label: base.label,
+      amount: base.amount,
+      currency: base.currency,
+      amountRwf: base.amountRwf,
+      category: base.category,
+      date: base.date,
+      received: base.received,
+      allocatedToSavingsRwf: base.allocatedToSavingsRwf,
+      remainingAvailableRwf: base.remainingAvailableRwf,
+      allocationStatus: base.allocationStatus,
+      createdBy: base.createdBy,
+      createdAt: base.createdAt,
+      updatedAt: base.updatedAt,
+      allocationCount: json['allocationCount'] as int? ?? 0,
+      savingAllocations:
+          ((json['savingAllocations'] as List<dynamic>?) ?? const [])
+              .map(
+                (item) => IncomeSavingAllocation.fromJson(
+                  item as Map<String, dynamic>,
+                ),
+              )
+              .toList(growable: false),
+    );
+  }
+
+  final int allocationCount;
+  final List<IncomeSavingAllocation> savingAllocations;
 }
 
 enum CurrencyCode {
@@ -105,6 +264,33 @@ enum IncomeCategory {
       'SIDE_HUSTLE' => IncomeCategory.sideHustle,
       'OTHER' => IncomeCategory.other,
       _ => throw StateError('Unsupported income category: $value'),
+    };
+  }
+}
+
+enum IncomeAllocationStatus {
+  unallocated,
+  partiallyAllocated,
+  fullyAllocated;
+
+  String get apiValue => switch (this) {
+    unallocated => 'UNALLOCATED',
+    partiallyAllocated => 'PARTIALLY_ALLOCATED',
+    fullyAllocated => 'FULLY_ALLOCATED',
+  };
+
+  String get displayName => switch (this) {
+    unallocated => 'Unallocated',
+    partiallyAllocated => 'Partially allocated',
+    fullyAllocated => 'Fully allocated',
+  };
+
+  static IncomeAllocationStatus fromApi(String value) {
+    return switch (value) {
+      'UNALLOCATED' => IncomeAllocationStatus.unallocated,
+      'PARTIALLY_ALLOCATED' => IncomeAllocationStatus.partiallyAllocated,
+      'FULLY_ALLOCATED' => IncomeAllocationStatus.fullyAllocated,
+      _ => throw StateError('Unsupported allocation status: $value'),
     };
   }
 }
