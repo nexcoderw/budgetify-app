7add508cd5f03908c38c960c60f43e8514d82c87
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-21 21:25:44 +0200

    Add Flutter expense summary audit and quote routes

--- lib/features/expenses/data/routes/expenses_api_routes.dart
+++ lib/features/expenses/data/routes/expenses_api_routes.dart
@@ -7,5 +7,8 @@ class ExpensesApiRoutes {
 
   String get list => _base;
   String get categories => '$_base/categories';
+  String get summary => '$_base/summary';
+  String get audit => '$_base/audit';
+  String get mobileMoneyQuote => '$_base/mobile-money/quote';
   String byId(String expenseId) => '$_base/$expenseId';
 }
